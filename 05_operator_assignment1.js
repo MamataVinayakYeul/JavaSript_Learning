48ffdbf37122f8aa6d96ac89b13f48a4c999669b
MamataYeul <mamatayeul9@gmail.com>
2022-10-13 02:10:52 +0530

    Assignments on operator files are added
    Assignments on operator file are added

---
+++ 05_operator_assignment1.js
@@ -0,0 +1,21 @@
+var wordLengthSquare=function(word){
+    var wordLength=word.length;
+    return wordLength*wordLength;
+
+ }
+ var word ="Javascript";
+ console.log(`The legth of "Javascript:"`,word.length);
+ console.log("The square of Javasript:",wordLengthSquare("JavaScript"));
+ console.log("==========================================================");
+ var word ="Google";
+ console.log(`The legth of "Google:"`,word.length);
+ console.log("The square of Google:", wordLengthSquare("Google"));
+ console.log("==========================================================");
+ var word ="Developer";
+ console.log(`The legth of "Developer:"`,word.length);
+ console.log("The square of Developer:", wordLengthSquare("Developer"));
+ console.log("==========================================================");
+ var string="I am Angular Developer";
+console.log(`The length of "I am a Angular Developer" is:`,string.length);
+console.log("Result=",string.length/4);
+console.log("Result=",string.length*4);
---
+++ 05_ternary_operator.js
@@ -0,0 +1,19 @@
+var num1=10;
+var result = num1 > 1 ? "Positive" : "Negative";
+console.log(result);
+var num1 = -5;
+ var result = num1 > 0 ? "Positive" : "Negative";
+ console.log(result);
+
+ 
+ var wordLengthSquare=function(){
+    var wordLength=wordLengthSquare.length;
+    return wordLength*wordLength;
+
+ }
+ console.log= wordLengthSquare("JavaScript");
+ wordLengthSquare("Google");
+ var givenStringLength="I am Angular Developer".length;
+ var result= "I am angular Develor".split(" length");
+ var total_words=result.length;
+ 
