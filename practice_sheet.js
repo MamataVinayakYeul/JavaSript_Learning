a5eb1c5673a2478043e782cd33f11bb3a2a794d4
MamataYeul <mamatayeul9@gmail.com>
2022-10-15 16:26:18 +0530

    Simple calculator design added to file
    Simple calculator design added to file

--- practice_sheet.js
+++ practice_sheet.js
@@ -1,6 +1,7 @@
+
 var wordLengthSquare=function show(arg1){
-var result=arg1.length();
-console.log()
+var result=arg1.length;
+console.log(`THe length of string is:`, result);
 }
 wordLengthSquare("Javascript");
 wordLengthSquare("Google");
